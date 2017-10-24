9b7a99cbd1d785da5dfd131b01b65b2f8dae5316
ConstanceEDJ <31658359+ConstanceEDJ@users.noreply.github.com>
2017-10-24 16:13:49 +0200

    cv

--- app.js
+++ app.js
@@ -1,45 +1,50 @@
 var app = new Vue({
   el: '#app',
   data: {
-    firstName: "Jules",
-    lastName: "Bonnard",
-    description: "Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
-    photo: "https://pbs.twimg.com/profile_images/899285838516936704/nfeLD7he_400x400.jpg",
-    twitter: "julesbonnard",
-    github: "julesbonnard",
-    mail: "julesbonnard@gmail.com",
+    firstName: "Constance",
+    lastName: "Daire",
+    description: "Etudiante en journalisme à Sciences Po",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/2/005/0b7/264/277ebec.jpg",
+    twitter: "constance_daire",
+    github: "ConstanceEDJ",
+    mail: "constance.daire@gmail.com",
     experiences: [{
-      dateBegin: "2014",
-      dateEnd: "Today",
-      name: "Agence France Presse",
-      title: "Datajournalist",
+      dateBegin: "Juin 2017",
+      dateEnd: "Juillet 2017",
+      name: "Libération",
+      title: "Stagiaire",
+      logo: "http://s1.libe.com/newsite/images/logo-libe.svg",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }, {
-      dateBegin: "Été 2013",
-      dateEnd: false,
-      name: "Le Monde.fr",
-      title: "Data-journalist",
-      description: false
+      dateBegin: "Avril 2017",
+      dateEnd: "Mai 2017",
+      name: "Neon",
+      title: "Stagiaire",
+      logo: "http://www.neonmag.fr/content/uploads/2016/06/cropped-LOGO-NEON-NOIR-2016-page-001.jpg",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }, {
-      dateBegin: "2012",
-      dateEnd: "2013",
-      name: "Le Dauphiné Libéré",
-      title: "Stagiaire et correspondant de presse",
-      description: false
+      dateBegin: "Décembre 2016",
+      dateEnd: "Mars 2017",
+      name: "L'Orient-Le Jour",
+      title: "Stagiaire",
+      logo: "https://s.olj.me/interface/french/images/logo_large.png?",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }],
     formations: [{
-      dateBegin: "2012",
-      dateEnd: "2014",
+      dateBegin: "2015",
+      dateEnd: false,
       name: "Master Journalisme",
-      university: "Sciences Po Grenoble"
+      logo: "http://www.sciencespo.fr/sites/default/files/logo%20site.jpg",
+      width: 30,
+      university: "Sciences Po Paris"
     }, {
-      dateBegin: "2009",
-      dateEnd: "2012",
+      dateBegin: "2012",
+      dateEnd: false,
       name: "Bachelor Sciences Politiques",
-      university: "Sciences Po Grenoble"
+      university: "Sciences Po Paris"
     }],
-    languages: ["Anglais"],
-    skills: ["Node", "VueJS", "D3js", "SCSS", "HTML"]
+    languages: ["Anglais", "Espagnol"],
+    skills: ["Adobe Photoshop", "Adobe Premiere",]
   },
   computed: {
     fullName() {
